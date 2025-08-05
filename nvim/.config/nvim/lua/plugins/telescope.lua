d4841a6758a646ca7cf91c060f805895d39720cb
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-08-05 09:02:43 -0400

    chore: update nvim plugin dependencies and configurations
    - Updated commit hashes for several plugins in lazy-lock.json.
    - Added 'yanky' plugin to lazyvim.json for enhanced clipboard management.
    - Enhanced telescope.lua with fzf-native dependency and configuration for improved file searching capabilities.

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,13 +1,19 @@
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make"
+    },
+  },
   keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
+    -- add a keymap to browse plugin files
+    -- stylua: ignore
+    {
+      "<leader>fp",
+      function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+      desc = "Find Plugin File",
+    },
 
     {
       "n",
@@ -29,6 +35,27 @@ return {
       layout_config = { prompt_position = "top" },
       sorting_strategy = "ascending",
       winblend = 0,
+      -- Disable treesitter highlighting
+      preview = {
+        treesitter = false
+      },
+    },
+    extensions = {
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+        case_mode = "smart_case",
+      },
     },
   },
+  -- Add config function to load extensions
+  config = function(_, opts)
+    require("telescope").setup(opts)
+    
+    -- Load fzf extension with error handling
+    pcall(function()
+      require("telescope").load_extension("fzf")
+    end)
+  end,
 }
