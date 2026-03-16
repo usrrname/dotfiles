172239198771d9b2ba6baa1c00bfa29c93622c37
Jen Chan <jenc@MacBook-Air-2.local>
2026-03-16 13:17:18 -0400

    refactor: remove old directories (now in stow/)
    - Remove old root directories (bash, nvim, zsh, etc.)
    - Remove old nix/ directory (config now in hosts/nixos/)
    - Files are now organized in stow/common/, stow/macos/, stow/linux/

--- nvim/.config/nvim/lua/config/options.lua
+++
@@ -1,7 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- vim.g.lazyvim_php_lsp = "intelephense"
-vim.g.mapleader = " "
-vim.g.autoformat = true
-vim.opt.clipboard = "unnamedplus"
-
