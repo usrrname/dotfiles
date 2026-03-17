c89987a27f9f8d442b211ff66fa1a1bd54d265cd
Jen Chan <jenc@MacBook-Air-2.local>
2026-03-17 17:15:41 -0400

    fix: use nixos-unstable and home-manager release-26.05
    - Change nixpkgs to nixos-unstable (nixos-26.05 doesn't exist)
    - Pin home-manager to release-26.05

--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
   description = "Dotfiles with Nix and Home Manager";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
