c2b7518586c4d12f7bc1ae240e56d961a4178a80
Jen Chan <jenc@MacBook-Air-2.local>
2026-03-17 17:31:07 -0400

    fix: use home-manager main branch (release branch doesn't exist)
    - Remove home-manager release pin
    - Keep nixos-unstable for nixpkgs

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
