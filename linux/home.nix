f134a124bfe1183e56157d6eefa4b39c73f05a90
Jen Chan <jenc@MacBook-Air-2.local>
2026-03-17 17:11:56 -0400

    fix: add home.homeDirectory to linux/home.nix
    - Add home.homeDirectory = "/home/jenc" (required by home-manager)

--- linux/home.nix
+++ linux/home.nix
@@ -62,6 +62,7 @@
   # =====================================================================
   home.stateVersion = "24.05";
   home.username = "jenc";
+  home.homeDirectory = "/home/jenc";
 
   home.sessionVariables = {
     EDITOR = "nvim";
