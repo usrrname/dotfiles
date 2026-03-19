181d0525c2286d96c635c3797e9331f8783a7231
Jen Chan <6406037+usrrname@users.noreply.github.com>
2026-03-19 12:12:29 -0400

    feat(nvim): disable trivial markdownlint rules
    Disable MD013 (line length) and MD026 (trailing punctuation)
    to reduce noise while editing markdown files.

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,3 +5,16 @@ vim.g.mapleader = " "
 vim.g.autoformat = true
 vim.opt.clipboard = "unnamedplus"
 
+-- disable markdownlint noise --
+vim.g.render_markdown = {
+	lint = {
+		enabled = true,
+		-- Disable specific rules
+		rules = {
+			-- Disable line length warnings
+			MD013 = false,
+			-- Disable trailing punctuation in headings
+			MD026 = false,
+		},
+	},
+}
