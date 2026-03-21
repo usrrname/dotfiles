f7feedf707502e9e46469e4babe49c4092fddd8a
Jen Chan <6406037+usrrname@users.noreply.github.com>
2026-03-21 13:23:51 -0400

    nvim: add Alt+Arrow keymaps to move lines

--- common/nvim/.config/nvim/lua/config/keymaps.lua
+++ common/nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Map Command+Shift+P (macOS) or Ctrl+Shift+P (Windows/Linux) to Telescope
-
--- macOS-style keybindings
-
-vim.api.nvim_set_keymap("n", "<D-a>", "ggVG", { desc = "Select all" })
 
 --- Save and append in insert mode --
 vim.keymap.set("i", "<D-s>", "<Esc><cmd>w<cr>a", { desc = "Save" })
@@ -23,3 +18,16 @@ end, { desc = "Reload Config" })
 
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+
+-- Move line up/down with Alt+Arrow
+vim.keymap.set("n", "<M-Up>", function()
+	vim.cmd("move -2")
+end, { desc = "Move line up" })
+
+vim.keymap.set("n", "<M-Down>", function()
+	vim.cmd("move +1")
+end, { desc = "Move line down" })
+
+-- Insert mode Alt+Arrow
+vim.keymap.set("i", "<M-Up>", "<Cmd>m .-2<CR>==gi", { desc = "Move line up" })
+vim.keymap.set("i", "<M-Down>", "<Cmd>m .+1<CR>==gi", { desc = "Move line down" })
