4f916a35e1f2c6300d08d6aab50d23e9b51e8027
Jen Chan <6406037+usrrname@users.noreply.github.com>
2026-05-22 10:10:32 -0400

    feat(nvim): add OSC 52 clipboard support for SSH and WezTerm
    - Configure vim.g.clipboard to use OSC 52 for copy/paste over SSH
    - Enables seamless clipboard integration in remote terminals

--- common/nvim/.config/nvim/init.lua
+++ common/nvim/.config/nvim/init.lua
@@ -3,7 +3,18 @@ require("config.lazy")
 local function augroup(name)
 	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
 end
-
+-- OSC 52 clipboard for SSH + Wezterm
+vim.g.clipboard = {
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+	},
+}
 vim.api.nvim_create_autocmd("VimEnter", {
 	group = augroup("startup"),
 	once = true,
