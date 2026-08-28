61b1a4e7e136480d4f9923f7f6f98d99672cd8a9
usrrname <6406037+usrrname@users.noreply.github.comn>
2026-08-28 11:21:33 -0400

    fmt

--- modules/tmux/tmux.nix
+++ modules/tmux/tmux.nix
@@ -1,12 +1,9 @@
-{
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   programs.tmux = {
     enable = true;
     terminal = "tmux-256color";
     escapeTime = 0;
-    plugins = with pkgs.tmuxPlugins; [ vim-tmux-navigator ];
+    plugins = with pkgs.tmuxPlugins; [vim-tmux-navigator];
     extraConfig = builtins.readFile ./tmux.conf;
   };
 }
