bafd62a226b76ee2854f736a83e9219781bccb82
Jen Chan <6406037+usrrname@users.noreply.github.com>
2026-09-12 10:19:41 -0400

    chore: add trouble.nvim plugin and fix starship config path
    - Add trouble.nvim to lazy.nvim plugins
    - Fix relative path in starship.nix config file source
    - Remove redundant comments from starship module

--- modules/starship/starship.nix
+++ modules/starship/starship.nix
@@ -1,12 +1,8 @@
 {
-  # Config is authored as raw TOML in ../common/starship/.config/starship.toml
-  # and deployed verbatim below (same pattern as ghostty/wezterm). `settings`
-  # is left empty so Home Manager doesn't generate its own config file, which
-  # would collide with the xdg.configFile definition.
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
   };
 
-  xdg.configFile."starship.toml".source = .config/starship.toml;
+  xdg.configFile."starship.toml".source = ./.config/starship.toml;
 }
